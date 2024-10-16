6f02f7e8a9afd25586465f7fc0e6538da7a0dd29
3a@hw <arbuzov.aa@inbox.ru>
2024-10-16 09:40:12 +0300

    fix: lint err for gendiff.test.js

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -1,14 +1,12 @@
-import genDiff from '../src/gendiff.js';
-
-import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import genDiff from '../src/gendiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-//const readFixtureFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+// const readFixtureFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const result = `{
   - follow: false
