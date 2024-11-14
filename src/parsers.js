1b466a2e8d3d65e2f1d3e784ce7d1ae96ee5b58b
3a@hw <arbuzov.aa@inbox.ru>
2024-11-14 19:14:19 +0300

    fix: parsers.js add: yml to yaml replacer

--- src/parsers.js
+++ src/parsers.js
@@ -3,14 +3,26 @@ import path from 'node:path';
 import yaml from 'js-yaml';
 
 const getFilePath = (filepath) => path.resolve(process.cwd(), filepath);
-const readFile = (filepath) => readFileSync(getFilePath(filepath), 'utf-8');
-
 const getExtention = (filepath) => path.extname(filepath);
 const getFileFormat = (filepath) => getExtention(filepath).slice(1);
 
+const replacerYmlToYaml = (filepath) => {
+  if (getFileFormat(filepath) === 'yml') {
+    const replaceExtention = /yml/gi;
+    return filepath.replace(replaceExtention, 'yaml');
+  }
+  return filepath;
+};
+
+const readFile = (filepath) => {
+  const replaceYmlToYaml = replacerYmlToYaml(filepath);
+  const filePath = getFilePath(replaceYmlToYaml);
+  return readFileSync(filePath);
+};
+
 export default (filepath) => {
-  const data = readFile(filepath);
   const fileFormat = getFileFormat(filepath);
+  const data = readFile(filepath);
 
   const parsers = {
     json: JSON.parse,
